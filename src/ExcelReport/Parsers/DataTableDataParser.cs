15656336c0e17bdd702ffb9809a0e719b864073e
qinfeng.zhu <qinfeng.zhu@ctsmed.com>
2020-03-18 12:06:12 +0800

    [add] 添加NPOI的模板导出对数据源 DataTable的支持,解决模板必须要字段一一对应的限制

---
+++ src/ExcelReport/Parsers/DataTableDataParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExcelReport.Parsers
+{
+    public class DataTableDataParser : RegexParser
+    {
+        private static readonly Regex regex = new Regex(@"(?<=\$\<)([\w]*)(?=\>\[data\])");
+
+        public override Regex Regex => regex;
+    }
+}
---
+++ src/ExcelReport/Parsers/DataTableHeaderParser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExcelReport.Parsers
+{
+    public sealed class DataTableHeaderParser:RegexParser
+    {
+        private static readonly Regex regex = new Regex(@"(?<=\$\<)([\w]*)(?=\>\[header\])");
+
+        public override Regex Regex => regex;
+    }
+}
